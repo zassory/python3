2585f3a514ba6bbbadf69e32c796a30a2a517c4e
nicolas <nicolas.programador@gmail.com>
2023-12-20 19:47:57 -0300

    dia 3

---
+++ index-rindex.py
@@ -0,0 +1,10 @@
+
+mi_texto = "Esta es una prueba"
+resultado = mi_texto.rindex("a")
+#                          bus des hasta
+#resultado = mi_texto.index("a",5,11)
+#resultado = mi_texto.index("prueba")
+#resultado = mi_texto.index("n")
+#resultado = mi_texto[-4]
+
+print(resultado)
---
+++ substring.py
@@ -0,0 +1,22 @@
+#        0123456789
+#texto = "ABCDEFGHIJKLM"
+#fragmento = texto[2:] Sin numero es hasta el final
+#fragmento = texto[:4] Lo mismo pero al reves
+#                  desde hasta  que se salte
+#fragmento = texto[2:10:2]
+#fragmento = texto[2:5]
+#fragmento = texto[::3]#No toma nada pero salta de 3 en 3
+#fragmento = texto[::-1]#Muestra la cadena al reves
+#print(fragmento)
+# frase = "Controlar la complejidad es la esencia de la programación"
+# resultado = frase[0:9]
+# print(resultado)
+
+# Pendiente
+# frase = "Nunca confíes en un ordenador que no puedas lanzar por una ventana"
+# resultado = frase[43:50]
+
+# print(resultado)
+frase = "Es genial trabajar con ordenadores. No discuten, lo recuerdan todo y no se beben tu cerveza"
+resultado = frase[::-1]
+print(resultado)
